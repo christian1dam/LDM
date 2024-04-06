bd757d69223bb1b7ad396a91baf8884107458c46
Christian Mamani Choque <christianmamani.dam@gmail.com>
2024-04-06 16:04:31 +0200

    updating repo

---
+++ Codigo/pruebaEvaluable/primera/pruebaEvaluable1Christianrmch.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a804d4968f718c85a64c275017aea7f2b1382aa00cf3faec12148b0fbe907e2f
+size 3062
---
+++ Codigo/pruebaEvaluable/senosycosenos.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:683b95cd8b77793dad078a201e5ba23e85c99fc2354b1cce16558749f290b59d
+size 1400
