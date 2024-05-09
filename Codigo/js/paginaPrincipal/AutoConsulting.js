7b794bdd118901fcf654bef7e1910d259fc12eca
Christian Mamani Choque <christianmamani.dam@gmail.com>
2024-05-09 17:00:31 +0200

    test

--- Codigo/js/paginaPrincipal/AutoConsulting.js
+++ Codigo/js/paginaPrincipal/AutoConsulting.js
@@ -1,3 +1,16 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:46335b03fdc32b87b32eab8235b5b581ec86bbf05049e4299ab05b792ed835c7
-size 501
+let icono = document.getElementById("menu_icono");
+
+icono.addEventListener("click", (e) => {
+    document.getElementById('lista_not_hover').style.left = "0px";
+});
+
+let main = document.getElementById("main");
+let footer = document.getElementById("footer");
+
+main.addEventListener('click', (e) => {
+    document.getElementById('lista_not_hover').style.left = "-100%";
+});
+
+footer.addEventListener('click', (e) => {
+    document.getElementById('lista_not_hover').style.left = "-100%";
+});
--- Codigo/pruebaEvaluable/pirmera/pruebaEvaluable1Christianrmch.js
+++ Codigo/pruebaEvaluable/pirmera/pruebaEvaluable1Christianrmch.js
@@ -1,3 +1,69 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a804d4968f718c85a64c275017aea7f2b1382aa00cf3faec12148b0fbe907e2f
-size 3062
+const MAX_TIMES_SELECTED_BUTTON = 3; 
+const MAX_PARTIDAS = 2; 
+var partidas = 0;
+var clicksOnTheButtonP1 = 0;
+var clicksOnTheButtonP2 = 0;
+var totalPuntosJugador1 = new Array();
+var totalPuntosJugador2 = new Array();
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.addEventListener('click', (element) => {
+        // comprueba que el click se ha hecho en un id específico
+        if(partidas < MAX_PARTIDAS){
+            if (element.target.id === 'tirarPlayer1' && clicksOnTheButtonP1 < MAX_TIMES_SELECTED_BUTTON) {
+                var dadosSeleccionadosEnTirada = document.getElementById('player1').value;
+                var tiradaDeDados = generarTiradaDeDados(dadosSeleccionadosEnTirada);
+                console.log(tiradaDeDados);
+                document.getElementById('puntosPlayer1').value = sumaDadosTirados(tiradaDeDados);
+                totalPuntosJugador1.push(document.getElementById('puntosPlayer1').value);
+                document.getElementById('totalPuntosPlayer1').value = sumarTotalPuntos(totalPuntosJugador1);
+                clicksOnTheButtonP1++;
+            } else if(element.target.id === 'tirarPlayer2' && clicksOnTheButtonP2 < MAX_TIMES_SELECTED_BUTTON) {
+                var dadosSeleccionadosEnTirada = document.getElementById('player1').value;
+                var tiradaDeDados = generarTiradaDeDados(dadosSeleccionadosEnTirada);
+                console.log(tiradaDeDados);
+                document.getElementById('puntosPlayer2').value = sumaDadosTirados(tiradaDeDados);
+                totalPuntosJugador2.push(document.getElementById('puntosPlayer2').value);
+                document.getElementById('totalPuntosPlayer2').value = sumarTotalPuntos(totalPuntosJugador2);
+                clicksOnTheButtonP2++;
+            } else {
+                console.log(`OTRO ELEMENTO clicado con ID: ${element.target.id}`);
+            }
+
+            if(clicksOnTheButtonP1 == 3 && clicksOnTheButtonP2 == 3){
+                partidas++;
+            }
+        }
+    });
+});
+
+
+function generarTiradaDeDados(dados){
+    var numeroDelDadoAlSerTirado = new Array();
+    for(var i = 0; i < dados; i++){
+        let tiradaAhora = Number.parseInt(Math.floor(Math.random() * 7));
+        if(tiradaAhora == 0) tiradaAhora = 1; 
+        numeroDelDadoAlSerTirado.push(tiradaAhora); 
+    }
+    return numeroDelDadoAlSerTirado;
+}
+
+function sumaDadosTirados(tiradaDeDados){
+    var sumaTotalDados = 0;
+    if(Array.isArray(tiradaDeDados) && tiradaDeDados.length > 0){
+        for(var i = 0; i < tiradaDeDados.length; i++){
+            sumaTotalDados += Number.parseInt(tiradaDeDados[i]); 
+        }
+    } else alert('No se han tirado dados elegidos');
+    return sumaTotalDados; 
+}
+
+function sumarTotalPuntos(arrayPuntos){
+    var sumaTotal = 0;
+    if(Array.isArray(arrayPuntos) && arrayPuntos.length > 0){
+        for(var i = 0; i < arrayPuntos.length; i++){
+            sumaTotal += Number.parseInt(arrayPuntos[i]); 
+        }
+    }
+    return sumaTotal; 
+}
