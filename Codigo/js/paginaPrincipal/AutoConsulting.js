4caac7a53a39deab83a6043fb3bac8975ece7b82
sumanlc <150013340+Sumanlc@users.noreply.github.com>
2024-05-10 10:42:00 +0200

    jsCookies
    añadir cookies.js paginaPrincipal

--- Codigo/js/paginaPrincipal/AutoConsulting.js
+++ Codigo/js/paginaPrincipal/AutoConsulting.js
@@ -14,3 +14,50 @@ main.addEventListener('click', (e) => {
 footer.addEventListener('click', (e) => {
     document.getElementById('lista_not_hover').style.left = "-100%";
 });
+
+
+//**Cookies**/
+
+document.addEventListener("DOMContentLoaded", function(){
+    var avisoCookies = document.getElementById("avisoCookies");
+    var cookiesForm = document.getElementById("cookiesForm");
+
+    if(!cookiesAceptadas()){
+        mostrarAvisoCookies();
+    }
+
+    function mostrarAvisoCookies(){
+        avisoCookies.style.display = "block";
+    }
+
+    function aceptarCookies(event){
+        event.preventDefault();
+        avisoCookies.style.display = "none";
+        almacenarCookiesAceptadas();
+    }
+
+    function rechazarCookies(event){
+        event.preventDefault();
+        avisoCookies.style.display = "none";
+    }
+
+    function cookiesAceptadas() {
+        return localStorage.getItem("cookiesAceptadas") === "true";
+    }
+
+    function almacenarCookiesAceptadas() {
+        var checkboxes = cookiesForm.querySelectorAll("input[type=checkbox]:checked");
+        localStorage.setItem("cookiesAceptadas", "true");
+        checkboxes.forEach(function(checkbox) {
+            localStorage.setItem(checkbox.name, "true");
+        });
+    }
+
+    cookiesForm.addEventListener("submit", aceptarCookies);
+    var rechazarButton = cookiesForm.querySelector('button[type="button"]');
+    rechazarButton.addEventListener("click", rechazarCookies);
+
+    if (cookiesAceptadas()) {
+        avisoCookies.style.display = "none";
+    }
+});
