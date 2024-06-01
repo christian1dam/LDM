2b37e1e94544ecf5d9fb5a3ccad5799076ece58c
Christian Mamani Choque <christianmamani.dam@gmail.com>
2024-06-02 01:27:01 +0200

    trabajo hasta el momento, error al coger la frase ocn la api, antes no pasaba, ahora si

--- Codigo/js/calendario/api.js
+++ Codigo/js/calendario/api.js
@@ -1,7 +1,7 @@
 export const getConsejo = () => {
     var xhr = new XMLHttpRequest(); //se instancia un objeto XMLHttpRrequest() para hacer la solicitud a la API.
 
-    xhr.open('GET', 'http://localhost:3000/api/phrase?' + new Date().getMilliseconds(), true);//se hace una peticion HTTP de tipo GET que consiste en obtener datos de la API
+    xhr.open('GET', 'http://localhost:3000/api/phrase?' + new Date().getDate(), true);//se hace una peticion HTTP de tipo GET que consiste en obtener datos de la API
 
     xhr.onload = function () {
         if (xhr.status === 200) {
@@ -10,10 +10,11 @@ export const getConsejo = () => {
             document.getElementById('autor').textContent = response.author;
         } else {
             console.log('Error al obtener la frase');
+            alert("eror");
         }
     }; //Se recoje los datos de la API y se trabaja con ellos. En este caso la api devuelve un archivo JSON con el que se debe trabajar. response = JSON.parse() transforma el JSON en contenido facilmente manipulable
 
-    xhr.send(); //envio de la peticion a la API. Se establece después del manejo de la respuesta por buenas practicas. 
+    xhr.send();
 }
 
 
--- Codigo/js/calendario/calendarioAutomoviles.js
+++ Codigo/js/calendario/calendarioAutomoviles.js
@@ -1,3 +1,28 @@
 import { getConsejo } from './api.js';
 
-getConsejo();
+
+const mesYanyo = document.getElementById('mostrarMesYAnyo');
+const calendario = document.getElementById('calendario');
+
+let fecha = new Date();
+let anyoActual = fecha.getFullYear();
+let mesActual = fecha.getMonth();
+
+const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+ "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+var cargarCalendario = () =>{
+    let ultimoDiaMes = new Date(anyoActual, mesActual + 1, 0).getDate();
+    let dias = "";
+    for (let i = 0; i < ultimoDiaMes; i++) {
+        dias += `<li>${i+1}</li>`;
+    }
+
+    mesYanyo.innerText = `${meses[mesActual]} ${anyoActual}`;
+    calendario.innerHTML = dias;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    getConsejo();
+    cargarCalendario();
+});
